7ddd149aef35f20670333970ddf1dac62cde0a95
p1eri <lightuponce@gmail.com>
2024-04-30 00:36:30 +0500

    link changes for tg

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
 			<footer className='startFooter'>
 				<article className='startFooterBottom'>
 					<p>В проект вложена<br></br>частичка моей души</p>
-					<Link className='navbarTelegram' to='https://t.me/pavlovdeveloper' target="_blank" rel="noopener noreferrer">Верстка сайта<br></br>@pavlovdeveloper</Link>
+					<Link className='navbarTelegram' to='https://t.me/p11baby' target="_blank" rel="noopener noreferrer">Верстка сайта<br></br>@p11baby</Link>
 				</article>
 			</footer>
 		)
@@ -32,8 +32,8 @@ const Footer = () => {
 					<Link className='footerLink' to='https://dprofile.ru/starflowdesign' target="_blank" rel="noopener noreferrer">DPROFILE</Link>
 					<Link className='footerLink' to='https://dribbble.com/StarflowDesign' target="_blank" rel="noopener noreferrer">DRIBBLE</Link>
 				</article>
-				<Link className='footerLinkSecond' to='https://t.me/pavlovdeveloper' target="_blank" rel="noopener noreferrer">Верстка
-					сайта<br></br>@pavlovdeveloper</Link>
+				<Link className='footerLinkSecond' to='https://t.me/p11baby' target="_blank" rel="noopener noreferrer">Верстка
+					сайта<br></br>@p11baby</Link>
 			</section>
 		</footer>
 	)
