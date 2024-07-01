f1bff2cd4b265594ac1a858a0270d285d79da91d
Pavel Ogaryshev <lightuponce@gmail.com>
2024-07-01 14:47:15 +0500

    Minor updates
    Footer component:
    - tsx/css improvements
    - add adaptive for main footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -40,10 +40,22 @@ const Footer = () => {
 		<footer className='mainFooter'>
       <Link to='https://t.me/StarflowDesign' className='goConversation'>
         <p className='mainFooterTop'><span className='difText'>Обсудим</span> <span className='secText'>Ваш проект</span></p>
+        <p className='mainFooterTopSecond'><span className='difText'>Обсудим</span><br></br><span className='secText'>Ваш проект</span></p>
         <div className='mainFooterP'><img className='leftArrow' src={arrowLeft} alt='arrow' />написать в телеграм<img className='rightArrow' src={arrowRight} alt='arrow' /></div>
       </Link>
 			<NavigationButtonsStatic/>
 			<NextPage />
+      <section className='mainFooterBottomSecond'>
+        <ul className='showLink'>
+					<Link className='ul_link' to='https://www.behance.net/StarflowDesign' target="_blank" rel="noopener noreferrer">BEHANCE</Link>
+					<Link className='ul_link' to='https://dprofile.ru/starflowdesign' target="_blank" rel="noopener noreferrer">DPROFILE</Link>
+					<Link className='ul_link' to='https://dribbble.com/StarflowDesign' target="_blank" rel="noopener noreferrer">DRIBBLE</Link>
+				</ul>
+        <section className='direct'>
+          <Link className='footerLinkFirst' to='mailto:starflowdesign@gmail.com' target="_blank" rel="noopener noreferrer">Рабочая почта<br></br>starflowdesign@gmail.com</Link>
+				  <Link className='footerLinkSecond' to='https://github.com/p1baby' target="_blank" rel="noopener noreferrer">Верстка сайта<br></br>@p1baby</Link>
+        </section>
+			</section>
 			<section className='mainFooterBottom'>
 				<Link className='footerLinkFirst' to='mailto:starflowdesign@gmail.com' target="_blank" rel="noopener noreferrer">Рабочая почта<br></br>starflowdesign@gmail.com</Link>
 				<ul className='showLink'>
