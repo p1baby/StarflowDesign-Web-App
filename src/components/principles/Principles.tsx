46c6559d16f5b275dfd9e0d64e770db97722c16d
p1baby <p1babywork@gmail.com>
2024-08-21 12:57:30 +0500

    Principles component updates

--- src/components/principles/Principles.tsx
+++ src/components/principles/Principles.tsx
@@ -1,34 +1,43 @@
-import ParallaxText from '../animation/velocity'
+import React, { useEffect, useState } from 'react'
+import ParallaxText from '../animations/velocity'
 import FooterPrinciples from '../footer/footerPrinciples/FooterPrinciples'
 import Navbar from '../navbar/Navbar'
 import './principles.scss'
-// const words = [
-//   ['Современность', 'Удобство', 'Понятность', 'Эстетика'],
-//   ['Новизна', 'Эффективность', 'Эмпатия', 'Качество'],
-//   ['Результат', 'Чистота', 'Эмоция', 'Функциональность']
-// ];
 
 interface PrinciplesProps {
-  handlePrinciplesShow: () => void;
+    handlePrinciplesShow: () => void;
 }
 
 const Principles: React.FC<PrinciplesProps> = ({ handlePrinciplesShow }) => {
-  return (
-    <section className='principlesSection'>
-      <Navbar/>
-      <section className='principlesShow'>
-          <section className='marquee'>
-              <ParallaxText baseVelocity={-5}>Современность / Удобство / Понятность / Эстетика /</ParallaxText>
-              <span className='principlesLine'></span>
-              <ParallaxText baseVelocity={5}>Новизна    /   Эффективность   /   Эмпатия   /   Качество    /</ParallaxText>
-              <span className='principlesLine'></span>
-              <ParallaxText baseVelocity={-5}>Результат / Чистота / Эмоция / Функциональность /</ParallaxText>
-          </section>
-      </section>
-      <FooterPrinciples handlePrinciplesShow={handlePrinciplesShow} />
-    </section>
-  );
-}
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true); // Запускаем анимацию появления при монтировании компонента
+    }, []);
+
+    const handleClose = () => {
+        setIsVisible(false); // Запускаем анимацию исчезновения
+        setTimeout(() => {
+            handlePrinciplesShow(); // Вызываем callback после завершения анимации
+        }, 500); // Время совпадает с длительностью анимации в CSS
+    };
+
+    return (
+        <section className={`principlesSection ${isVisible ? 'fadeIn' : 'fadeOut'}`}>
+            <Navbar />
+            <section className='principlesShow'>
+                <section className='marquee'>
+                    <ParallaxText baseVelocity={-5}>Современность / Удобство / Понятность / Эстетика /</ParallaxText>
+                    <span className='principlesLine'></span>
+                    <ParallaxText baseVelocity={5}>Новизна / Эффективность / Эмпатия / Качество /</ParallaxText>
+                    <span className='principlesLine'></span>
+                    <ParallaxText baseVelocity={-5}>Результат / Чистота / Эмоция / Функциональность /</ParallaxText>
+                </section>
+            </section>
+            <FooterPrinciples handlePrinciplesShow={handleClose} />
+        </section>
+    );
+};
 
 export default Principles;
 // const words = [
