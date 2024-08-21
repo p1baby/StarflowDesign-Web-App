a9635961fb4816891d858f08c514737d32353586
p1baby <p1babywork@gmail.com>
2024-08-21 13:50:04 +0500

    FooterPrinciples fix

--- src/components/footer/footerPrinciples/FooterPrinciples.tsx
+++ src/components/footer/footerPrinciples/FooterPrinciples.tsx
@@ -2,29 +2,29 @@ import { Link } from 'react-router-dom'
 import './footerPrinciples.scss'
 
 interface PrinciplesProps {
-  handlePrinciplesShow: () => void;
+    handlePrinciplesShow: () => void;
 }
 
 const FooterPrinciples: React.FC<PrinciplesProps> = ({ handlePrinciplesShow }) => {
     return (
-      <footer className='principlesFooter'>
-        <section className='principlesDesktop'>
-            <Link to='https://dprofile.ru/starflowdesign/cv' target="_blank" rel="noopener noreferrer">Резюме CV</Link>
-              <p className='navbarText'>
-                Для того, чтобы сделать что-то <br></br>интересное, нужно это полюбить.
-              </p>
-            <button onClick={handlePrinciplesShow}>Закрыть</button>
-        </section>
-        <footer className='principlesMobile'>
+        <footer className='principlesFooter'>
+            <section className='principlesDesktop'>
+                <Link to='https://dprofile.ru/starflowdesign/cv' target="_blank" rel="noopener noreferrer">Резюме CV</Link>
+                <p className='navbarText'>
+                    Для того, чтобы сделать что-то <br></br>интересное, нужно это полюбить.
+                </p>
+                <button onClick={handlePrinciplesShow}>Закрыть</button>
+            </section>
+            <footer className='principlesMobile'>
                 <p className='navbarTextMobile'>
-                  Для того, чтобы сделать что-то <br></br>интересное, нужно это полюбить.
+                    Для того, чтобы сделать что-то <br></br>интересное, нужно это полюбить.
                 </p>
                 <section className='principlesMobile'>
-                  <Link to='https://dprofile.ru/starflowdesign/cv' target="_blank" rel="noopener noreferrer">Резюме CV</Link>
-                  <button onClick={handlePrinciplesShow}>Закрыть</button>
+                    <Link to='https://dprofile.ru/starflowdesign/cv' target="_blank" rel="noopener noreferrer">Резюме CV</Link>
+                    <button onClick={handlePrinciplesShow}>Закрыть</button>
                 </section>
-          </footer>
-      </footer>
+            </footer>
+        </footer>
     );
 }
 
