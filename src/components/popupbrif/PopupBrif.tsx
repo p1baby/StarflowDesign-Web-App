45c688264e8b02187424246948355076c5f7f8f1
p1baby <p1babywork@gmail.com>
2024-08-27 00:03:02 +0500

    Add new text animation PopupBrif, NavigationButtons animation fix

--- src/components/popupbrif/PopupBrif.tsx
+++ src/components/popupbrif/PopupBrif.tsx
@@ -1,4 +1,5 @@
 import { useForm } from '@mantine/form'
+import { motion } from 'framer-motion'
 import { useOutsideClick } from '../popupbrif/outsideClick/useOutsideClick'
 
 import { sendMessage } from '../../api/telegram'
@@ -7,67 +8,74 @@ import useTimer from '../hooks/useTimer'
 import { useEffect, useState } from 'react'
 
 
+import { popupAnimation } from '../animations/modals'
+import useSplittingHover from '../hooks/useSplittingHover'
 import './popupbrif.scss'
 import SuccessContent from './successContent/SuccessContent'
 import TimerContent from './timerContent/TimerContent'
 
 interface Props {
-  onClose: () => void;
-  selectedService: string | null;
+    onClose: () => void;
+    selectedService: string | null;
 }
 
 function PopupBrif(props: Props) {
-  const { ref } = useOutsideClick(props.onClose);
-  const [selectedProjectType, setSelectedProjectType] = useState<string | null>(null);
-  const [selectedProjectBudget, setSelectedProjectBudget] = useState<string | null>(null);
-  const [showDefaultContent, setShowDefaultContent] = useState(true);
-  const [showSuccessContent, setShowSuccessContent] = useState(false);
-  const [showThirdContent, setShowThirdContent] = useState(false);
-  const { startTimer, stopTimer, timerActive } = useTimer(0);
+    const { ref } = useOutsideClick(props.onClose);
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+    const [selectedProjectType, setSelectedProjectType] = useState<string | null>(null);
+    const [selectedProjectBudget, setSelectedProjectBudget] = useState<string | null>(null);
+    const [showDefaultContent, setShowDefaultContent] = useState(true);
+    const [showSuccessContent, setShowSuccessContent] = useState(false);
+    const [showThirdContent, setShowThirdContent] = useState(false);
+    const { startTimer, stopTimer, timerActive } = useTimer(0);
 
-  useEffect(() => { //функция для опредления типа проекта в модальном окне
-    setSelectedProjectType(projectButtons.find(button => button.id === props.selectedService)?.value || null);
-}, [props.selectedService]);
+    useEffect(() => {
+      setDrawerIsOpen(true);
+    }, []);
 
-useEffect(() => { //функция для отключения overflow во время того как открыто то или иное модальное окно
-  if (showDefaultContent || showSuccessContent || showThirdContent) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = '';
-  }
+    useEffect(() => { //функция для опредления типа проекта в модальном окне
+        setSelectedProjectType(projectButtons.find(button => button.id === props.selectedService)?.value || null);
+    }, [props.selectedService]);
 
-  return () => {
-    document.body.style.overflow = '';
-  };
-}, [showDefaultContent, showSuccessContent, showThirdContent]);
+    useEffect(() => { //функция для отключения overflow во время того как открыто то или иное модальное окно
+    if (showDefaultContent || showSuccessContent || showThirdContent) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = '';
+    }
 
-useEffect(() => {
-  const storedShowThirdContent = localStorage.getItem('showThirdContent');
-  if (storedShowThirdContent === 'true') {
-    setShowThirdContent(true);
-    setShowDefaultContent(false);
-    setShowSuccessContent(false);
-    startTimer();
-  }
-}, [startTimer]);
+    return () => {
+        document.body.style.overflow = '';
+    };
+    }, [showDefaultContent, showSuccessContent, showThirdContent]);
 
-useEffect(() => {
-  if (showThirdContent) {
-    startTimer();
-  } else {
+    useEffect(() => {
+        const storedShowThirdContent = localStorage.getItem('showThirdContent');
+        if (storedShowThirdContent === 'true') {
+            setShowThirdContent(true);
+            setShowDefaultContent(false);
+            setShowSuccessContent(false);
+            startTimer();
+        }
+    }, [startTimer]);
+
+    useEffect(() => {
+    if (showThirdContent) {
+        startTimer();
+    } else {
     stopTimer();
-  }
-}, [showThirdContent, startTimer, stopTimer]);
+    }
+    }, [showThirdContent, startTimer, stopTimer]);
 
-const form = useForm({
+    const form = useForm({
 		mode: 'uncontrolled',
 		initialValues: {
-		  name: '',
-		  contacts: '',
-		  projectType: '',
-		  projectBudget: '',
-		  projectDetails: '',
-		  projectLink: '',
+		    name: '',
+		    contacts: '',
+		    projectType: '',
+		    projectBudget: '',
+		    projectDetails: '',
+		    projectLink: '',
 		},
 
 		validate: {
@@ -109,49 +117,57 @@ const form = useForm({
 
   const handleFormSubmit = async ({ name, contacts, projectType, projectBudget, projectDetails, projectLink }: typeof form.values) => {
     try {
-      const date = new Date();
-      const options: Intl.DateTimeFormatOptions = {
-        year: '2-digit',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false,
-        timeZone: 'Asia/Yekaterinburg',
-      };
-      const formatter = new Intl.DateTimeFormat('ru-RU', options);
-      const formattedTime = formatter.format(date);
+        const date = new Date();
+        const options: Intl.DateTimeFormatOptions = {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+            timeZone: 'Asia/Yekaterinburg',
+        };
+        const formatter = new Intl.DateTimeFormat('ru-RU', options);
+        const formattedTime = formatter.format(date);
 
-      const message = `⚠️ НОВЫЙ ЗАКАЗ от ${formattedTime}⚠️\n\nИмя клиента: ${name},\nКонтакт: ${contacts},\nТип проекта: ${projectType},\nБюджет проекта: ${projectBudget},\nДетали проекта:\n${projectDetails}\nСсылка на редизайн: ${projectLink}`;
+        const message = `⚠️ НОВЫЙ ЗАКАЗ от ${formattedTime}⚠️\n\nИмя клиента: ${name},\nКонтакт: ${contacts},\nТип проекта: ${projectType},\nБюджет проекта: ${projectBudget},\nДетали проекта:\n${projectDetails}\nСсылка на редизайн: ${projectLink}`;
 
-      await sendMessage(message);
-      setShowDefaultContent(false); // Hide default
-      setShowSuccessContent(true);
+        await sendMessage(message);
+        setShowDefaultContent(false); // Hide default
+        setShowSuccessContent(true);
     } catch (e) {
-      form.setFieldError('projectLink', e as string);
-      console.error('Error', e);
+        form.setFieldError('projectLink', e as string);
+        console.error('Error', e);
     }
   };
 
-  const handleDrawerClose = () => {
+    const handleDrawerClose = () => {
+      
     if (showSuccessContent) {
-      setShowSuccessContent(false);
-      setShowThirdContent(true);
-      localStorage.setItem('showThirdContent', 'true');
-      startTimer();
+        setShowSuccessContent(false);
+        setShowThirdContent(true);
+        localStorage.setItem('showThirdContent', 'true');
+        startTimer();
     } else if (showThirdContent) {
-      setShowThirdContent(false);
-      stopTimer();
-      props.onClose(); // Close the drawer when the button is clicked
+        setShowThirdContent(false);
+        stopTimer();
+        props.onClose();
     }
   };
+
+    useSplittingHover();
 
   return (
-    <section className='overlay'>
+    <section className={`overlay ${drawerIsOpen ? 'visible' : ''}`}>
       {showDefaultContent ? (
         <>
-           <section className='drawer' ref={ref}>
+           <motion.section
+           variants={popupAnimation}
+           animate='enter'
+           exit='exit'
+           initial='initial'
+           className='drawer' ref={ref}>
             <header>
               <hgroup>
                 <h5 className='startText'>Начнём<br />Сотрудничество!</h5>
@@ -220,9 +236,9 @@ const form = useForm({
                 </div>
                 {form.errors.projectDetails && <p className='errorText'>{form.errors.projectDetails}</p>}
               </fieldset>
-              <button type='submit' className='submitButton'>Отправить</button>
+              <button data-splitting type='submit' className='submitButton'>Отправить</button>
             </form>
-          </section>
+          </motion.section>
         </>
       ) : showSuccessContent ? (
         <SuccessContent ref={ref} handleDrawerClose={handleDrawerClose} />
