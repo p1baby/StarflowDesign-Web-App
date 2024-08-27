70ec17332cca583c188e11df0ad6a423a7d37357
p1baby <p1babywork@gmail.com>
2024-08-28 00:42:04 +0500

    OpenBurger code improvements

--- src/components/navbar/openBurger/openBurger.tsx
+++ src/components/navbar/openBurger/openBurger.tsx
@@ -1,20 +1,16 @@
-import { useEffect, useState } from 'react'
+import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import Splitting from 'splitting'
 import './openBurger.scss'
+import logo from '/logoSV.svg'
 
 import { useContext } from 'react'
 import { BurgerContext } from '../Navbar'
 
-
-import { AnimatePresence } from 'framer-motion'
-// import { FadeText } from '../../animations/fadeTextBurger'
 import PopupBrif from '../../popupbrif/PopupBrif'
 
 const openBurger = () => {
     const setIsBurgerOpen = useContext(BurgerContext);
-    const [brifOpened, setBrifOpened] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
+    const [brifOpened, setBrifOpened] = React.useState(false);
     const location = useLocation();
 
     const contextClick = () => {
@@ -30,53 +26,32 @@ const openBurger = () => {
     const pageText = {
         '/projects': 'Проекты',
         '/services': 'Услуги',
-        '/about': 'Обо  мне',
+        '/about': 'Обо мне',
         '/contacts': 'Контакты',
     };
 
-    useEffect(() => {
-        // const results = 
-        Splitting();
-        // console.log(results); // Debug: See what elements are affected by Splitting.js
-
-        setTimeout(() => {
-            setIsVisible(true);
-        }, 700); // delay
-    }, []);
-
-    return (
-        <>
-            <AnimatePresence mode='wait'>
-                {brifOpened && <PopupBrif onClose={() => setBrifOpened(false)} selectedService={null} />}
-            </AnimatePresence>
-            <section className='burgerContent'>
-                <nav>
-                    {Object.entries(pageText).map(([path, text]) => (
-                        <Link
-                            key={path}
-                            onClick={contextClick}
-                            className={`navLink ${location.pathname === path ? 'italicText' : ''}`}
-                            to={path}
-                            >
-                            <section className={`linkContent ${isVisible ? 'visible' : ''}`} data-splitting>
-                                {location.pathname === path && (
-                                    <img
-                                        src={`/arrowLeft.svg`}
-                                        alt='Left Image'
-                                        className={isVisible ? 'visible' : ''}
-                                    />
-                                )}
-                                {text}
-                                {location.pathname === path && (
-                                    <img
-                                        src={`/arrowRight.svg`}
-                                        alt='Right Image'
-                                        className={isVisible ? 'visible' : ''}
-                                    />
-                                )}
-                            </section>
-                        </Link>
-                    ))}
+	return(
+		<>
+        {brifOpened && <PopupBrif onClose={() => setBrifOpened(false)} selectedService={null} /> }
+        <header className='headerMenu'>
+            <Link to='/' onClick={contextClick} className='headerBtn'> <img src={logo} alt='logo' />Starflow<br></br>Design</Link>
+        </header>
+        <section className='burgerContent'>
+            <nav>
+                {Object.entries(pageText).map(([path, text]) => (
+                    <Link
+                        key={path}
+                        onClick={contextClick}
+                        className={`navLink ${location.pathname === path ? 'italicText' : ''}`}
+                        to={path}
+                    >
+                        <section className="linkContent">
+                            {location.pathname === path && <img src={`/arrowLeft.svg`} alt='Left Image' />}
+                            {text}
+                            {location.pathname === path && <img src={`/arrowRight.svg`} alt='Right Image' />}
+                        </section>
+                    </Link>
+                ))}
                 </nav>
             <section className='lowerSection'>
                 <article>
@@ -91,14 +66,14 @@ const openBurger = () => {
                         <img className='contactLight' alt='light' src='/burgerLight.svg' />
                     </a>
                 </article>
-                <ul className={`ulPortfolio ${isVisible ? 'visible' : ''}`}>
+                <ul className='ulPortfolio'>
                     <Link className='portfolioLink' to='https://www.behance.net/StarflowDesign' target="_blank" rel="noopener noreferrer">BEHANCE</Link>
                     <Link className='portfolioLink' to='https://dprofile.ru/starflowdesign' target="_blank" rel="noopener noreferrer">DPROFILE</Link>
                     <Link className='portfolioLink' to='https://dribbble.com/StarflowDesign' target="_blank" rel="noopener noreferrer">DRIBBLE</Link>
                 </ul>
                 <footer>
                     <Link className='footerLinkFirst' to='mailto:starflowdesign@gmail.com' target="_blank" rel="noopener noreferrer">Рабочая почта<br />starflowdesign@gmail.com</Link>
-                    <ul className={`links ${isVisible ? 'visible' : ''}`}>
+                    <ul>
                         <Link className='portfolioLink' to='https://www.behance.net/StarflowDesign' target="_blank" rel="noopener noreferrer">BEHANCE</Link>
                         <Link className='portfolioLink' to='https://dprofile.ru/starflowdesign' target="_blank" rel="noopener noreferrer">DPROFILE</Link>
                         <Link className='portfolioLink' to='https://dribbble.com/StarflowDesign' target="_blank" rel="noopener noreferrer">DRIBBLE</Link>
