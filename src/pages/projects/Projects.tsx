77e9261e236eab72d0f919a6227d04abf52152f6
p1baby <p1babywork@gmail.com>
2024-08-29 17:49:03 +0500

    project prepare

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,64 +1,63 @@
-import { useEffect, useRef, useContext, useState } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import './projects.scss';
+// import gsap from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import './projects.scss'
 
-import TransitionContext from '../../components/contextGsap/TransitionContext';
+// import TransitionContext from '../../components/contextGsap/TransitionContext'
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 const Projects: React.FC = () => {
 
 
-  const main = useRef<HTMLElement | null>(null);
-  const context = useContext(TransitionContext);
-  const scrollTween = useRef<gsap.core.Tween | null>(null);
-  const snapTriggers = useRef<ScrollTrigger[]>([]);
+  // const main = useRef<HTMLElement | null>(null);
+  // const context = useContext(TransitionContext);
+  // const scrollTween = useRef<gsap.core.Tween | null>(null);
+  // const snapTriggers = useRef<ScrollTrigger[]>([]);
 
-  useEffect(() => {
-    if (!context || !main.current) return;
+  // useEffect(() => {
+  //   if (!context || !main.current) return;
 
-    const panels: HTMLElement[] = gsap.utils.toArray('.panel', main.current) as HTMLElement[];
-    let scrollStarts: number[] = [0];
-    let snapScroll = (value: number, direction: number) => value + direction;
+  //   const panels: HTMLElement[] = gsap.utils.toArray('.panel', main.current) as HTMLElement[];
+  //   let scrollStarts: number[] = [0];
+  //   let snapScroll = (value: number, direction: number) => value + direction;
 
-    panels.forEach((panel, i) => {
-      snapTriggers.current[i] = ScrollTrigger.create({
-        trigger: panel,
-        start: 'top top',
-      });
-    });
+  //   panels.forEach((panel, i) => {
+  //     snapTriggers.current[i] = ScrollTrigger.create({
+  //       trigger: panel,
+  //       start: 'top top',
+  //     });
+  //   });
 
-    ScrollTrigger.addEventListener('refresh', () => {
-      scrollStarts = snapTriggers.current.map((trigger) => trigger.start);
-      snapScroll = ScrollTrigger.snapDirectional(scrollStarts);
-    });
+  //   ScrollTrigger.addEventListener('refresh', () => {
+  //     scrollStarts = snapTriggers.current.map((trigger) => trigger.start);
+  //     snapScroll = ScrollTrigger.snapDirectional(scrollStarts);
+  //   });
 
-    ScrollTrigger.observe({
-      type: 'wheel,touch',
-      onChangeY(self) {
-        if (!scrollTween.current) {
-          const direction = self.deltaY > 0 ? 0.1 : -1; // Уменьшите значение deltaY
-          const scroll = snapScroll(self.scrollY(), direction * Math.abs(self.deltaY));
-          goToSection(scrollStarts.indexOf(scroll));
-        }
-      },
-    });
-    ScrollTrigger.refresh();
-  }, [context]);
+  //   ScrollTrigger.observe({
+  //     type: 'wheel,touch',
+  //     onChangeY(self) {
+  //       if (!scrollTween.current) {
+  //         const direction = self.deltaY > 0 ? 0.1 : -1; // Уменьшите значение deltaY
+  //         const scroll = snapScroll(self.scrollY(), direction * Math.abs(self.deltaY));
+  //         goToSection(scrollStarts.indexOf(scroll));
+  //       }
+  //     },
+  //   });
+  //   ScrollTrigger.refresh();
+  // }, [context]);
 
-  const goToSection = (i: number) => {
-    console.log('scroll to', i);
-    scrollTween.current = gsap.to(window, {
-      scrollTo: { y: snapTriggers.current[i].start as number, autoKill: false },
-      duration: 1,
-      onComplete: () => (scrollTween.current = null),
-      overwrite: true,
-    });
-  };
+  // const goToSection = (i: number) => {
+  //   console.log('scroll to', i);
+  //   scrollTween.current = gsap.to(window, {
+  //     scrollTo: { y: snapTriggers.current[i].start as number, autoKill: false },
+  //     duration: 1,
+  //     onComplete: () => (scrollTween.current = null),
+  //     overwrite: true,
+  //   });
+  // };
 
   return (
-    <main ref={main}>
+    <main>
       <section className="panel">
           <img className="bg" src='/projectsBg1.png'/>
       </section>
