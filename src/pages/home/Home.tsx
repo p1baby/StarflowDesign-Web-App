194eef7b5bd1f4197d95b5af9f7cdddfc98589dd
p1baby <p1babywork@gmail.com>
2024-08-30 21:05:06 +0500

    Home Page add new animations

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,44 +6,68 @@ import NavigationButtons from '../../components/navigationButtons/NavigationButt
 import './home.scss'
 
 function Home() {
-	const [isVisible, setIsVisible] = useState(false);
+    const [visibleLines, setVisibleLines] = useState<number[]>([]);
 
-	useSplittingOnLoad('.slide-vertical');
+    useSplittingOnLoad('.slide-vertical');
 
-	useEffect(() => {
-		// Apply Splitting.js to the desired elements
-		Splitting({ target: '.homeText p' });
-	
-		// Delay setting visibility
-		setTimeout(() => {
-			setIsVisible(true);
-		}, 5000); // Adjust delay as needed
-	}, []);
+    useEffect(() => {
+        Splitting({ target: '.homeText p' });
+        
+        const initialDelay = 3500; // Задержка перед появлением первой строки
+        const subsequentDelay = 500; // Задержка между последующими строками
 
-	return (
-		<Curve>
-			<section className='homeSection'>
-			<h1 data-splitting className='slide-vertical'><span className='firText'>Star<span className='ligatures'>fl</span><span className='owFollowing'>ow</span></span><span className='secText'> Design</span></h1>
-				<section className='welcomeUnder'>
-					<span className='bracket1'>(</span>
-					<p className='navbarText'>
-						Открыт для любого<br />сотрудничества и предложений
-					</p>
-					<span className='bracket2'>)</span>
-				</section>
-				<NavigationButtons/>
-				<article className='homeText'>
-					<p className={`${isVisible ? 'visible' : ''}`} data-splitting>
-						<span className='char'>Эмпатичный дизайнер, стремлюсь</span><br /><span className='char'>сделать ваш бренд понятным</span><br /><span className='char'>и выделяющимся</span>
+        const lines = document.querySelectorAll('.homeText p');
+        lines.forEach((line, index) => {
+            const delay = initialDelay + subsequentDelay * index;
+            setTimeout(() => {
+                setVisibleLines(prev => [...prev, index]);
+            }, delay);
+        });
+    }, []);
+
+    return (
+        <Curve>
+            <section className='homeSection'>
+                <h1 data-splitting className='slide-vertical'>
+                    <span className='firText'>Star<span className='ligatures'>fl</span><span className='owFollowing'>ow</span></span>
+                    <span className='secText'> Design</span>
+                </h1>
+                <section className='welcomeUnder'>
+                    <span className='bracket1'>(</span>
+                    <p className='navbarText'>
+                        Открыт для любого<br />сотрудничества и предложений
                     </p>
-                    <p className={`${isVisible ? 'visible' : ''}`} data-splitting>
-                        <span className='char'>Моя цель —  создание эффектных,</span><br /><span className='char'>и эффективных решений</span><br /><span className='char'>для вашего бизнеса</span>
-                    </p>
-				</article>
-			</section>
-		</Curve>
-	);
+                    <span className='bracket2'>)</span>
+                </section>
+                <NavigationButtons/>
+                <article className='homeText'>
+					<section>
+						<p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
+							<span className='char'>Эмпатичный дизайнер, стремлюсь</span>
+						</p>
+						<p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
+							<span className='char'>сделать ваш бренд понятным</span>
+						</p>
+						<p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
+							<span className='char'>и выделяющимся</span>
+						</p>
+					</section>
+
+					<section>
+						<p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
+							<span className='char'>Моя цель — создание эффектных</span>
+						</p>
+						<p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
+							<span className='char'>и эффективных решений</span>
+						</p>
+						<p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
+							<span className='char'>для вашего бизнеса</span>
+						</p>
+					</section>
+                </article>
+            </section>
+        </Curve>
+    );
 }
 
 export default Home;
-
