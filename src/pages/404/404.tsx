697d0c8b1e5fd2de2eebb53dba39036b56f26b5e
p1baby <p1babywork@gmail.com>
2024-09-06 23:33:13 +0500

    Page 404 add new animations

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,23 +1,35 @@
 import { Link } from 'react-router-dom'
 import NavigationButtons from '../../components/navigationButtons/NavigationButtons.tsx'
 //import star404 from '/star404.svg';
+import useSplittingOnLoad from '../../components/hooks/useSplittingOnLoad.tsx'
+import Curve from '../../components/layoutTransition/index.tsx'
 import './404.scss'
 import arrowLeft from '/arrowLeft.svg'
 import arrowRight from '/arrowRight.svg'
 
 function Undefpage() {
+
+	useSplittingOnLoad('.slide-vertical');
+	
 	return (
-        <>
+        <Curve>
         <section className='undefSection'>
-					<h1><span className='firText'>Страница</span><span className='secText'> не найдена</span></h1>
+					<h1 data-splitting className='slide-vertical'>
+						<span className='firText'>Страница</span>
+						<span className='secText'> не найдена</span>
+					</h1>
 					<h1 className='second'><span className='firText'>Страница</span><br></br><span className='secText'>не найдена</span></h1>
 					<NavigationButtons/>
-					<Link to='/' className='undefText'>
+					<Link
+					data-splitting
+					to='/' 
+					className='undefText'
+					>
 						<p>Похоже, Вас унесло<br></br>звездным потоком</p>
 						<section className='undefFooterLink'><img className='leftArrow' src={arrowLeft} alt='arrow' />вернуться на главную<img className='rightArrow' src={arrowRight} alt='arrow' /></section>
 					</Link>
 			</section>
-        </>
+        </Curve>
 	);
 }
 
