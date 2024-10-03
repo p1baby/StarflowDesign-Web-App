3122be05cc3e9d3727a5cf0a687754847eeca9d6
p1baby <p1babywork@gmail.com>
2024-10-03 21:49:11 +0300

    SuccessContent component code improvements

--- src/components/popupbrif/successContent/SuccessContent.tsx
+++ src/components/popupbrif/successContent/SuccessContent.tsx
@@ -10,16 +10,16 @@ interface SuccessContentProps {
 const SuccessContent = forwardRef<HTMLElement, SuccessContentProps>(({ handleDrawerClose }, ref) => {
 	return (
       <article className='drawerSecond' ref={ref}>
-          <header>
-            <p className='popupTextSecond'>Вместе мы сделаем что-то<br />по истине крутое</p>
-          </header>
-          <main>
-            <h4 className='successMessage'>Спасибо! Ваша заявка<br />получена!</h4>
-          </main>
-          <footer>
-            <p className='footerFormText'>Я изучу бриф и свяжусь с Вами<br />в ближайшее время</p>
-            <button onClick={handleDrawerClose} className='closeButton2'><img className='leftArrow' src={arrowLeft} alt='arrow' />Закрыть<img className='rightArrow' src={arrowRight} alt='arrow' /></button>
-          </footer>
+            <header>
+                <p className='popupTextSecond'>Вместе мы сделаем что-то<br />по истине крутое</p>
+            </header>
+            <main>
+                <h4 className='successMessage'>Спасибо! Ваша заявка<br />получена!</h4>
+            </main>
+            <footer>
+                <p className='footerFormText'>Я изучу бриф и свяжусь с Вами<br />в ближайшее время</p>
+                <button onClick={handleDrawerClose} className='closeButton2'><img className='leftArrow' src={arrowLeft} alt='arrow' />Закрыть<img className='rightArrow' src={arrowRight} alt='arrow' /></button>
+            </footer>
         </article>
 	);
 })
