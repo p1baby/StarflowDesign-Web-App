403a373cd82d55183b6436c4e5ea2f82c93ece34
p1baby <p1babywork@gmail.com>
2024-10-03 21:12:31 +0300

    Project Page removed old code or added comments for development

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,73 +1,38 @@
-// import gsap from 'gsap'
-// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import {
+  MotionValue,
+  useScroll,
+  useTransform
+} from "framer-motion"
+import { useRef } from "react"
 import './projects.scss'
 
-// import TransitionContext from '../../components/contextGsap/TransitionContext'
-
-// gsap.registerPlugin(ScrollTrigger);
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
 
 const Projects: React.FC = () => {
-
-
-  // const main = useRef<HTMLElement | null>(null);
-  // const context = useContext(TransitionContext);
-  // const scrollTween = useRef<gsap.core.Tween | null>(null);
-  // const snapTriggers = useRef<ScrollTrigger[]>([]);
-
-  // useEffect(() => {
-  //   if (!context || !main.current) return;
-
-  //   const panels: HTMLElement[] = gsap.utils.toArray('.panel', main.current) as HTMLElement[];
-  //   let scrollStarts: number[] = [0];
-  //   let snapScroll = (value: number, direction: number) => value + direction;
-
-  //   panels.forEach((panel, i) => {
-  //     snapTriggers.current[i] = ScrollTrigger.create({
-  //       trigger: panel,
-  //       start: 'top top',
-  //     });
-  //   });
-
-  //   ScrollTrigger.addEventListener('refresh', () => {
-  //     scrollStarts = snapTriggers.current.map((trigger) => trigger.start);
-  //     snapScroll = ScrollTrigger.snapDirectional(scrollStarts);
-  //   });
-
-  //   ScrollTrigger.observe({
-  //     type: 'wheel,touch',
-  //     onChangeY(self) {
-  //       if (!scrollTween.current) {
-  //         const direction = self.deltaY > 0 ? 0.1 : -1; // Уменьшите значение deltaY
-  //         const scroll = snapScroll(self.scrollY(), direction * Math.abs(self.deltaY));
-  //         goToSection(scrollStarts.indexOf(scroll));
-  //       }
-  //     },
-  //   });
-  //   ScrollTrigger.refresh();
-  // }, [context]);
-
-  // const goToSection = (i: number) => {
-  //   console.log('scroll to', i);
-  //   scrollTween.current = gsap.to(window, {
-  //     scrollTo: { y: snapTriggers.current[i].start as number, autoKill: false },
-  //     duration: 1,
-  //     onComplete: () => (scrollTween.current = null),
-  //     overwrite: true,
-  //   });
-  // };
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 3);
 
   return (
-    <main>
-      <section className="panel">
-          <img className="bg" src='/projectsBg1.png'/>
-      </section>
-      <section className="panel">
-        <img className="bg" src='/projectsBg2.png'/>
-      </section>
-      <section className="panel">
-        <img className="bg" src='/projectsBg3.png'/>
-      </section>
-    </main>
+    <section className='projectSection'>
+    {/* <div ref={ref}> */}
+      {/* <motion.section className="panel">
+        <motion.img className="bg" src='/projectsBg1.png' style={{ y }}/>
+      </motion.section>
+      <motion.section className="panel">
+        <motion.img className="bg" src='/projectsBg2.png' style={{ y }}/>
+      </motion.section>
+      <motion.section className="panel">
+        <motion.img className="bg" src='/projectsBg3.png' style={{ y }}/>
+      </motion.section> */}
+      {/* <img className='bg' src='/projectsBg1.png'/>
+      <img className='bg' src='/projectsBg2.png'/>
+      <img className='bg' src='/projectsBg3.png'/>
+    </div> */}
+    1
+    </section>
   );
 }
 
